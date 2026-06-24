3cf48c12ee0a6f6e3942726e95cecf2b78cb34e2
svnshsahu <shivanshshahganj@gmail.com>
2026-06-24 15:24:51 +0530

    Updates

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import conf from './conf/config'; 
+import conf from './conf/conf'; 
 function App() {
     console.log(conf.appwriteUrl);
     // react strict mode mein hai islie 2 bar aa raha 
